d237ada432739a32671140a308067fa114df4fb0
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-04-04 22:14:31 -0600

    Pues, ya reconoce el boton jaja

--- app/src/main/java/com/example/guatemala/MainActivity.kt
+++ app/src/main/java/com/example/guatemala/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.guatemala
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +15,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    fun mostrarTexto(view:View){
+        val boton:ImageButton = findViewById<ImageButton>(R.id.BotonEstrella)
+        val ingresdo:TextView = findViewById<TextView>(R.id.NombreIngresado)
+        boton.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this, "No puede disminuir abajo de cero", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+
+
+
+
 }
