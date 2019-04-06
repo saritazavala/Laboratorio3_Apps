5a553d90d2fbc4d7b77470991baea264147ef35e
saritazavala <42972998+saritazavala@users.noreply.github.com>
2019-04-05 23:27:13 -0600

    Comentarios

--- Laboratorio3_Apps-master/app/src/main/java/com/example/guatemala/Descripcion.kt
+++ Laboratorio3_Apps-master/app/src/main/java/com/example/guatemala/Descripcion.kt
@@ -2,6 +2,12 @@ package com.example.guatemala
 
 import android.os.Bundle
 import java.io.Serializable
+/*Sara Zavala 18893
+Programación de Apps mobiles
+Laboratorio 3: Guatemala
+Vuernes 05 de Abril 2019 */
+
+//data class creada como en los ejemplos en clase
 
 data class Descripcion(var nombre:String,var desc:String, var frase:String) : Serializable {
 
